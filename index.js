e3916ee23004e0b708e567b68f9f81715d2b8dd4
Phoz07 <115572920+Phoz07@users.noreply.github.com>
2024-06-05 21:14:35 +0700

    add something to index.js

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Hello Node")
-})
+app.get("/", (req, res) => {
+  res.send("Hello Node");
+});
 
-app.get('/about', (req, res) => {
-    res.send("Hello About Page")
-})
+app.get("/about", (req, res) => {
+  res.send("Hello About Page");
+});
+
+app.get("/products", (req, res) => {
+  res.send("Hello Product Page");
+});
 
 app.listen(8000, () => {
   console.log("Server is running");
